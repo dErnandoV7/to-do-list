8cca6dc2f769bb1e93a5223d9112b91afca0cbf0
dErnandoV7 <143457523+dErnandoV7@users.noreply.github.com>
2023-10-15 10:39:18 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -14,16 +14,14 @@ const defaultInput = document.createElement('input')
 defaultInput.setAttribute('type', 'checkbox')
 defaultInput.setAttribute('class', 'check')
 
-const defaultSpan = document.createElement('span')
-defaultInput.setAttribute('type', 'checkbox')
-defaultInput.setAttribute('class', 'check')
+const defaultLabel = document.createElement('label')
 
 const defaultImg = document.createElement('img')
 defaultImg.setAttribute('src', 'img/x.svg')
 defaultImg.setAttribute('class', 'delete')
 
 defaultTarefaContent.appendChild(defaultInput)
-defaultTarefaContent.appendChild(defaultSpan)
+defaultTarefaContent.appendChild(defaultLabel)
 defaultTarefa.appendChild(defaultTarefaContent)
 defaultTarefa.appendChild(defaultImg)
 
@@ -31,7 +29,9 @@ defaultTarefa.appendChild(defaultImg)
 const valueTarefa = document.querySelector('.novaTarefa')
 const button = document.querySelector(".buttonAdd");
 
-button.onclick = () => {
+button.onclick = (event) => {
+  event.preventDefault()
+  
   if (!valueTarefa.value) return
 
   const newTarefa = defaultTarefa.cloneNode(true)
@@ -39,7 +39,12 @@ button.onclick = () => {
 
   newTarefa.classList.add(`tarefa-${contadorTarefa}`)
   newTarefaContent.querySelector('input').classList.add(`check-${contadorTarefa}`)
-  newTarefaContent.querySelector('span').textContent = valueTarefa.value
+  const newLabel = newTarefaContent.querySelector('label')
+  const newInput = newTarefaContent.querySelector('input')
+  newLabel.textContent = valueTarefa.value
+  newLabel.setAttribute('for', `check${contadorTarefa}`)
+  newInput.setAttribute('id', `check${contadorTarefa}`)
+
   boxTarefas.appendChild(newTarefa)
   console.log(newTarefa)
   valueTarefa.value = ''
